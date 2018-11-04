c9b7c72db38e0c83f3ba1db9ca3fe054cf7b63f4
EduBic <bic.studio@yahoo.it>
2018-11-04 22:31:30 +0100

    Learned Helper classes

--- src/app/babylon-container/babylon-container.component.ts
+++ src/app/babylon-container/babylon-container.component.ts
@@ -4,66 +4,66 @@ import {
   AfterViewInit
 } from '@angular/core';
 
-import * as BABYLON from 'babylonjs';
+// import * as BABYLON from 'babylonjs';
 
-class Game {
-  private _canvas: HTMLCanvasElement;
-  private _engine: BABYLON.Engine;
-  private _scene: BABYLON.Scene;
-  private _camera: BABYLON.FreeCamera;
-  private _light: BABYLON.Light;
+// class Game {
+//   private _canvas: HTMLCanvasElement;
+//   private _engine: BABYLON.Engine;
+//   private _scene: BABYLON.Scene;
+//   private _camera: BABYLON.FreeCamera;
+//   private _light: BABYLON.Light;
 
-  constructor(canvasElement: string) {
-    // Create canvas and engine.
-    this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
-    this._engine = new BABYLON.Engine(this._canvas, true);
-  }
+//   constructor(canvasElement: string) {
+//     // Create canvas and engine.
+//     this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
+//     this._engine = new BABYLON.Engine(this._canvas, true);
+//   }
 
-  createScene(): void {
-    // Create a basic BJS Scene object.
-    this._scene = new BABYLON.Scene(this._engine);
+//   createScene(): void {
+//     // Create a basic BJS Scene object.
+//     this._scene = new BABYLON.Scene(this._engine);
 
-    // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-    this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), this._scene);
+//     // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
+//     this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), this._scene);
 
-    // Target the camera to scene origin.
-    this._camera.setTarget(BABYLON.Vector3.Zero());
+//     // Target the camera to scene origin.
+//     this._camera.setTarget(BABYLON.Vector3.Zero());
 
-    // Attach the camera to the canvas.
-    this._camera.attachControl(this._canvas, false);
+//     // Attach the camera to the canvas.
+//     this._camera.attachControl(this._canvas, false);
 
-    // Create a basic light, aiming 0,1,0 - meaning, to the sky.
-    this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
+//     // Create a basic light, aiming 0,1,0 - meaning, to the sky.
+//     this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
 
-    // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
-    const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {
-      segments: 16,
-      diameter: 2
-    }, this._scene);
+//     // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
+//     const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {
+//       segments: 16,
+//       diameter: 2
+//     }, this._scene);
 
-    // Move the sphere upward 1/2 of its height.
-    sphere.position.y = 1;
+//     // Move the sphere upward 1/2 of its height.
+//     sphere.position.y = 1;
 
-    // Create a built-in "ground" shape.
-    const ground = BABYLON.MeshBuilder.CreateGround('ground', {
-      width: 6,
-      height: 6,
-      subdivisions: 2
-    }, this._scene);
-  }
+//     // Create a built-in "ground" shape.
+//     const ground = BABYLON.MeshBuilder.CreateGround('ground', {
+//       width: 6,
+//       height: 6,
+//       subdivisions: 2
+//     }, this._scene);
+//   }
 
-  doRender(): void {
-    // Run the render loop.
-    this._engine.runRenderLoop(() => {
-      this._scene.render();
-    });
+//   doRender(): void {
+//     // Run the render loop.
+//     this._engine.runRenderLoop(() => {
+//       this._scene.render();
+//     });
 
-    // The canvas/window resize event handler.
-    window.addEventListener('resize', () => {
-      this._engine.resize();
-    });
-  }
-}
+//     // The canvas/window resize event handler.
+//     window.addEventListener('resize', () => {
+//       this._engine.resize();
+//     });
+//   }
+// }
 
 @Component({
   selector: 'app-babylon-container',
@@ -79,16 +79,16 @@ export class BabylonContainerComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    window.addEventListener('DOMContentLoaded', () => {
-      // Create the game using the 'renderCanvas'.
-      this.game = new Game('renderCanvas');
+    // window.addEventListener('DOMContentLoaded', () => {
+    //   // Create the game using the 'renderCanvas'.
+    //   this.game = new Game('renderCanvas');
 
-      // Create the scene.
-      this.game.createScene();
+    //   // Create the scene.
+    //   this.game.createScene();
 
-      // Start render loop.
-      this.game.doRender();
-    });
+    //   // Start render loop.
+    //   this.game.doRender();
+    // });
   }
 
 }
--- src/app/gui-container/gui-container.component.ts
+++ src/app/gui-container/gui-container.component.ts
@@ -3,11 +3,15 @@ import {
   OnInit,
   ViewChild,
   ElementRef,
-  AfterViewInit
+  AfterViewInit,
+  HostListener,
+  Testability
 } from '@angular/core';
 
 import * as THREE from 'three';
-import { OrbitControls } from 'three-orbitcontrols-ts';
+import {
+  OrbitControls
+} from 'three-orbitcontrols-ts';
 
 @Component({
   selector: 'app-gui-container',
@@ -20,27 +24,63 @@ export class GuiContainerComponent implements AfterViewInit {
   wrapperElement: ElementRef;
 
   scene: THREE.Scene;
-  camera;
   renderer;
+
+  camera;
+  cameraHelper;
+
+  directorCamera;
+  activeCamera;
+
+  cameraRig;
 
   mesh;
   cube;
   line;
   points: THREE.Points[];
 
-  controls;
+  directorCameraRotateX: number;
+
+  frameCounter = 0;
 
   constructor() {}
 
   ngAfterViewInit() {
     // Create scene
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.renderer = new THREE.WebGLRenderer();
+
+    // Set Cameras
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.z = 5;
+    this.camera.position.y = 1;
+
+    this.activeCamera = this.camera;
+
+    this.cameraHelper = new THREE.CameraHelper(this.camera);
+    // this.scene.add(this.cameraHelper);
+
+
+    this.directorCamera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 4);
+    this.directorCamera.position.y += 4;
+    this.directorCamera.rotation.x = 270 * Math.PI / 180;
+
+
+    const dirCameraHelper = new THREE.CameraHelper(this.directorCamera);
+    this.scene.add(dirCameraHelper);
+
+    this.cameraRig = new THREE.Group();
+    this.cameraRig.add(this.camera);
+    this.cameraRig.add(this.directorCamera);
+    this.scene.add(this.cameraRig);
+
+    // const directorCameraHelper = new THREE.CameraHelper( this.directorCamera );
+    // this.scene.add(directorCameraHelper);
 
     // console.log(window.innerWidth, window.innerHeight);
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setSize(window.innerWidth - 100, window.innerHeight - 100);
+
     this.wrapperElement.nativeElement.appendChild(this.renderer.domElement);
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -48,45 +88,49 @@ export class GuiContainerComponent implements AfterViewInit {
       color: 0x00ff00
     });
     this.cube = new THREE.Mesh(geometry, material);
-    this.scene.add(this.cube);
+    // this.scene.add(this.cube);
 
     const lineMaterial = new THREE.LineBasicMaterial({
       color: 0x0000ff
     });
 
     const lineGeometry = new THREE.Geometry();
-    lineGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
     lineGeometry.vertices.push(new THREE.Vector3(0, 1, 0));
+    lineGeometry.vertices.push(new THREE.Vector3(1, 1, 0));
     lineGeometry.vertices.push(new THREE.Vector3(1, 0, 0));
     lineGeometry.vertices.push(new THREE.Vector3(2, 0, 0));
-    // lineGeometry.vertices.push(new THREE.Vector3( 1, 0, 0) );
 
     this.line = new THREE.Line(lineGeometry, lineMaterial);
-    // this.scene.add(this.line);
+    this.scene.add(this.line);
 
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    // controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
-    this.controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-    this.controls.dampingFactor = 0.25;
-    this.controls.screenSpacePanning = false;
-    this.controls.minDistance = 100;
-    this.controls.maxDistance = 500;
-    this.controls.maxPolarAngle = Math.PI / 2;
+    const gridColor = new THREE.Color('#c0c0c0');
+    const gridHelper = new THREE.GridHelper(10, 10, gridColor);
+    this.scene.add(gridHelper);
+
+    // HELPER ZONE
+    const axesHelper = new THREE.AxesHelper(5);
+    this.scene.add(axesHelper);
+
+    const linePerspGeo = new THREE.Geometry();
+    linePerspGeo.vertices.push(new THREE.Vector3(0, 4, 0));
+    linePerspGeo.vertices.push(new THREE.Vector3(0, 4, -5));
+
+    const linePersp = new THREE.Line(linePerspGeo, lineMaterial);
+    this.scene.add(linePersp);
 
     // Add a point
-    const dotGeometry = new THREE.Geometry();
-    dotGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
-    dotGeometry.vertices.push(new THREE.Vector3(1, 1, 1));
-    dotGeometry.vertices.push(new THREE.Vector3(1, 1, 0));
-    const dotMaterial = new THREE.PointsMaterial({
-      size: 1,
-      sizeAttenuation: false
-    });
+    // const dotGeometry = new THREE.Geometry();
+    // dotGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+    // dotGeometry.vertices.push(new THREE.Vector3(1, 1, 1));
+    // dotGeometry.vertices.push(new THREE.Vector3(1, 1, 0));
+    // const dotMaterial = new THREE.PointsMaterial({
+    //   size: 1,
+    //   sizeAttenuation: false
+    // });
 
-    const dot = new THREE.Points(dotGeometry, dotMaterial);
+    // const dot = new THREE.Points(dotGeometry, dotMaterial);
     // this.scene.add(dot);
 
-    this.camera.position.z = 5;
 
     // animate
     this.animate();
@@ -104,12 +148,72 @@ export class GuiContainerComponent implements AfterViewInit {
   private animate(): void {
     window.requestAnimationFrame(() => this.animate());
 
-    // this.cube.rotation.x += 0.01;
-    // this.cube.rotation.y += 0.01;
+    // this.line.rotation.x += 0.01;
+    this.line.rotation.y += 0.01;
+    // this.directorCamera.rotation.x -= 0.005;
 
-    this.controls.update();
+
+    if (this.frameCounter % 60 === 0) {
+      console.log('TAC');
+      // this.directorCamera.rotation.y -= 2 * Math.PI / 180;
+      // this.directorCamera.rotation.x -= 2 * Math.PI / 180;
+      // this.directorCamera.rotation.x += 100;
+      this.directorCameraRotateX = this.directorCamera.rotation.x;
+    }
+    this.frameCounter++;
+
+    // if (this.activeCamera === this.camera) {
+    //   this.camera.updateProjectionMatrix();
+    //   this.cameraHelper.update();
+
+    // } else {
+    //   this.directorCamera.updateProjectionMatrix();
+    // }
 
     this.renderer.render(this.scene, this.camera);
+  }
+
+  @HostListener('document:keydown.ArrowLeft', ['$event'])
+  goLeft(event: KeyboardEvent) {
+    console.log('goLeft');
+    this.camera.position.x -= 0.05;
+  }
+
+  @HostListener('document:keydown.ArrowRight', ['$event'])
+  goRight(event: KeyboardEvent) {
+    console.log('goRight');
+    this.camera.position.x += 0.05;
+  }
+
+  @HostListener('document:keydown.ArrowUp', ['$event'])
+  goAhead(event: KeyboardEvent) {
+    this.camera.position.z -= 0.05;
+  }
+
+  @HostListener('document:keydown.ArrowDown', ['$event'])
+  goBack(event: KeyboardEvent) {
+    this.camera.position.z += 0.05;
+  }
+
+  @HostListener('document:keydown.q', ['$event'])
+  goUp(event: KeyboardEvent) {
+    // console.log('goUp');
+    this.camera.position.y += 0.05;
+  }
+
+  @HostListener('document:keydown.a', ['$event'])
+  goDown(event: KeyboardEvent) {
+    // console.log('goDown');
+    this.camera.position.y -= 0.05;
+  }
+
+  setNormalView() {
+    console.log('Normal');
+    this.activeCamera = this.camera;
+  }
+  setAngleView() {
+    console.log('Angle');
+    this.activeCamera = this.directorCamera;
   }
 
 }
